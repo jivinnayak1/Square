930e93750d4b071bd7823f1bc31181d573254e8e
Jivin Nayak <jivin.nayak@gmail.com>
2024-11-17 17:12:04 +0530

    Add files via upload

---
+++ square.py
@@ -0,0 +1,24 @@
+import turtle
+
+turtle.Screen().bgcolor("blue")
+turtle.Screen().setup(500,300)
+pen = turtle.Turtle()
+pen.up()
+pen.goto(50,50)
+pen.down()
+pen.forward(100)
+pen.right(90)
+pen.forward(100)
+pen.right(90)
+pen.forward(100)
+pen.right(90)
+pen.forward(100)
+
+
+
+
+
+
+
+
+turtle.done()
